17e3d32a490c9a8520c2d437c94f56c14cbb565f
Huahai Yang <huahai.yang@gmail.com>
2014-11-09 01:18:40 -0800

    don't create db if data directory is not empty

--- docker-entrypoint-initdb.d/init.sh
+++ docker-entrypoint-initdb.d/init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 gosu postgres pg_ctl -w start 
-gosu postgres psql -q <<-EOF
-  CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD'; 
-  ALTER USER postgres WITH PASSWORD '$POSTGRES_PASSWORD';
-EOF
-gosu postgres createdb -O $POSTGRES_USER $POSTGRES_DB
+if [ -z "$(ls -A "$PGDATA")" ]; then
+  gosu postgres psql -q <<-EOF
+    CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD'; 
+    ALTER USER postgres WITH PASSWORD '$POSTGRES_PASSWORD';
+  EOF
+  gosu postgres createdb -O $POSTGRES_USER $POSTGRES_DB
+fi
 if [ -n "$S3_BACKUP_DIR" ]; then 
   gosu postgres envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-push "$PGDATA"
 fi
