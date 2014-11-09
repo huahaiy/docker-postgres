fc59e85ed7de78a4e2596839decf317ff90d6293
Huahai Yang <huahai.yang@gmail.com>
2014-11-09 00:07:49 -0800

    backup base

--- docker-entrypoint-initdb.d/init.sh
+++ docker-entrypoint-initdb.d/init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 gosu postgres pg_ctl -w start 
 gosu postgres psql -q <<-EOF
   CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD'; 
@@ -8,3 +7,6 @@ gosu postgres psql -q <<-EOF
 EOF
 gosu postgres createdb -O $POSTGRES_USER $POSTGRES_DB
 gosu postgres pg_ctl stop 
+if [ -n "$S3_BACKUP_DIR" ]; then 
+  envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-push "$PGDATA"
+fi
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,11 +8,6 @@ if [ "$1" = 'postgres' ]; then
     { echo; echo 'host all all 0.0.0.0/0 md5'; } >> "$PGDATA"/pg_hba.conf
     pgtune -T Web -i "$PGDATA"/postgresql.conf > "$PGDATA"/local.conf
     { echo; echo "include = 'local.conf'"; } >> "$PGDATA"/postgresql.conf
-    if [ -d /docker-entrypoint-initdb.d ]; then
-      for f in /docker-entrypoint-initdb.d/*.sh; do
-        [ -f "$f" ] && . "$f"
-      done
-    fi
   fi
   if [ -n "$S3_BACKUP_DIR" ]; then 
     umask u=rwx,g=rx,o=
@@ -24,8 +19,13 @@ if [ "$1" = 'postgres' ]; then
     chown -R root:postgres /etc/wal-e.d
     { echo; echo "wal_level = archive"; } >> "$PGDATA"/postgresql.conf
     { echo; echo "archive_mode = on"; } >> "$PGDATA"/postgresql.conf
-    { echo; echo "archive_command = 'envdir /etc/wal-e.d/env /usr/local/bin/wal-e wal-push %p'"; } >> "$PGDATA"/postgresql.conf
+    { echo; echo "archive_command = 'envdir /etc/wal-e.d/env /usr/local/bin/wal-e --terse wal-push %p'"; } >> "$PGDATA"/postgresql.conf
     { echo; echo "archive_timeout = 60"; } >> "$PGDATA"/postgresql.conf
+  fi
+  if [ -d /docker-entrypoint-initdb.d ]; then
+    for f in /docker-entrypoint-initdb.d/*.sh; do
+      [ -f "$f" ] && . "$f"
+    done
   fi
   exec gosu postgres "$@"
 fi
